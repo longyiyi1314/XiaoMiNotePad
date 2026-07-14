586d3954b43589e269ec657ac381db8cf1424979
XiaoMiNote Dev <xiaominote@example.com>
2026-07-14 16:49:32 +0000

    Simplify build config: remove kotlin-compose plugin, use composeOptions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
 pluginManagement {
     repositories {
+        mavenLocal()
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
@@ -12,6 +15,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
