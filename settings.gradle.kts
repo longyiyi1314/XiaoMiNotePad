2854dc93cd0b391571fd690e6a97ce5012a5c7a3
XiaoMiNote Dev <xiaominote@example.com>
2026-07-14 13:35:22 +0000

    Restore official Maven repositories for GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,16 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        mavenLocal()
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        mavenLocal()
+        google()
+        mavenCentral()
     }
 }
 
