c5a6e3dac173f80125b5e02b6e619b7e82f4b740
XiaoMiNote Dev <xiaominote@example.com>
2026-07-14 17:08:37 +0000

    Fix Compose Compiler compatibility: downgrade Kotlin to 1.9.24

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
-        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.9.25-1.0.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.9.24-1.0.20")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
     }
 }
